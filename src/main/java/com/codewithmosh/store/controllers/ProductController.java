9b288e7e01e28136c3ec02d73d50b012b06d2a20
mandarbagwe7 <bagwemandar7@gmail.com>
2025-09-18 03:25:34 +0100

    UserAndProductControllers

---
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -0,0 +1,51 @@
+package com.codewithmosh.store.controllers;
+
+import com.codewithmosh.store.dtos.ProductDto;
+import com.codewithmosh.store.entities.Category;
+import com.codewithmosh.store.entities.Product;
+import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    @GetMapping
+    List<ProductDto> getProducts(
+           @RequestParam(required = false, name = "categoryId") Byte categoryId
+    ){
+        List<Product> products;
+        if(categoryId != null){
+            products = productRepository.findByCategoryId(categoryId);
+        }
+        else{
+            products = productRepository.findAllWithCategory();
+        }
+
+        return products
+                .stream()
+                .map(productMapper::toDto)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    ResponseEntity<ProductDto> getProduct(@PathVariable Long id){
+        var product = productRepository.findById(id).orElse(null);
+
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(productMapper.toDto(product));
+    }
+}
