d1eee116ac97cdf25578930c40ce61f1f645d970
mandarbagwe7 <bagwemandar7@gmail.com>
2025-09-24 02:02:04 +0100

    CartOperationsAndSwagger

--- src/main/java/com/codewithmosh/store/controllers/CartController.java
+++ src/main/java/com/codewithmosh/store/controllers/CartController.java
@@ -3,90 +3,104 @@ package com.codewithmosh.store.controllers;
 import com.codewithmosh.store.dtos.AddItemToCartRequest;
 import com.codewithmosh.store.dtos.CartDto;
 import com.codewithmosh.store.dtos.CartItemDto;
-import com.codewithmosh.store.entities.Cart;
-import com.codewithmosh.store.entities.CartItem;
-import com.codewithmosh.store.mappers.CartMapper;
-import com.codewithmosh.store.repositories.CartRepository;
-import com.codewithmosh.store.repositories.ProductRepository;
+import com.codewithmosh.store.dtos.UpdateCartItemRequest;
+import com.codewithmosh.store.exceptions.CartItemNotFoundException;
+import com.codewithmosh.store.exceptions.CartNotFoundException;
+import com.codewithmosh.store.exceptions.ProductNotFoundException;
+import com.codewithmosh.store.services.CartService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.UUID;
 
 @AllArgsConstructor
 @RestController
 @RequestMapping("/carts")
+@Tag(name = "Carts")
 public class CartController {
 
-    private final CartRepository cartRepository;
-    private final CartMapper cartMapper;
-    private final ProductRepository productRepository;
+    private final CartService cartService;
 
     @PostMapping
     public ResponseEntity<CartDto> createCart(
             UriComponentsBuilder uriBuilder
     ) {
-        var cart = new Cart();
-        cartRepository.save(cart);
-
-        var cartDto = cartMapper.toDto(cart);
+        var cartDto = cartService.createCart();
         var uri = uriBuilder.path("/carts/{id}").buildAndExpand(cartDto.getId()).toUri();
-
         return ResponseEntity.created(uri).body(cartDto);
     }
 
     @PostMapping("/{cart-id}/items")
+    @Operation(summary = "Adds an item to the cart.")
     public ResponseEntity<CartItemDto> addToCart(
-            @PathVariable(name = "cart-id") UUID cart_id,
+            @Parameter(description = "ID of the cart.")
+            @PathVariable(name = "cart-id") UUID cartId,
             @RequestBody AddItemToCartRequest request
     ){
-        var cart = cartRepository.getCartWithItems(cart_id).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.notFound().build();
-        }
-
-        var product = productRepository.findById(request.getProductId()).orElse(null);
-        if (product == null) {
-            return ResponseEntity.badRequest().build();
-        }
-
-        var cartItem = cart.getItems().stream()
-                .filter(item -> item.getProduct().getId().equals(product.getId()))
-                .findFirst()
-                .orElse(null);
-
-        if(cartItem != null) {
-            cartItem.setQuantity(cartItem.getQuantity() + 1);
-        }
-        else {
-            cartItem = new CartItem();
-            cartItem.setProduct(product);
-            cartItem.setQuantity(1);
-            cartItem.setCart(cart);
-            cart.getItems().add(cartItem);
-        }
-
-        cartRepository.save(cart);
-
-        var cartItemDto = cartMapper.toDto(cartItem);
-
+        var cartItemDto = cartService.addToCart(cartId, request.getProductId());
         return ResponseEntity.status(HttpStatus.CREATED).body(cartItemDto);
-
     }
 
     @GetMapping("/{cart-id}")
-    public ResponseEntity<CartDto> getCart(
-            @PathVariable(name = "cart-id") UUID cart_id
+    public CartDto getCart(
+            @PathVariable(name = "cart-id") UUID cartId
     ){
-        var cart = cartRepository.getCartWithItems(cart_id).orElse(null);
-        if (cart == null) {
-            return ResponseEntity.notFound().build();
-        }
+        return cartService.getCart(cartId);
+    }
 
-        return ResponseEntity.ok(cartMapper.toDto(cart));
+    @PutMapping("/{cartId}/items/{productId}")
+    public CartItemDto updateItem(
+            @PathVariable("cartId") UUID cartId,
+            @PathVariable("productId") Long productId,
+            @Valid @RequestBody UpdateCartItemRequest request
+    ){
+        return cartService.updateItem(cartId, productId, request.getQuantity());
+    }
+
+    @DeleteMapping("/{cartId}/items/{productId}")
+    public ResponseEntity<?> deleteCart(
+            @PathVariable("cartId") UUID cartId,
+            @PathVariable("productId") Long productId
+    ){
+        cartService.deleteCart(cartId, productId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @DeleteMapping("/{cartId}/items")
+    public ResponseEntity<?> clearCart(
+            @PathVariable("cartId") UUID cartId
+    ){
+        cartService.clearCart(cartId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @ExceptionHandler(CartNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerCartNotFound(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                Map.of("Error", "Cart Not Found.")
+        );
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerProductNotFound(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                Map.of("Error", "Product Not Found.")
+        );
+    }
+
+    @ExceptionHandler(CartItemNotFoundException.class)
+    public ResponseEntity<Map<String, String>> handlerCartItemNotFound(){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                Map.of("Error", "Item Not Found in the Cart.")
+        );
     }
 
 }
--- src/main/java/com/codewithmosh/store/entities/Cart.java
+++ src/main/java/com/codewithmosh/store/entities/Cart.java
@@ -23,13 +23,47 @@ public class Cart {
     @Column(name = "date_created", insertable = false, updatable = false)
     private LocalDate dateCreated;
 
-    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE)
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.MERGE, orphanRemoval = true)
     private Set<CartItem> items = new LinkedHashSet<>();
 
     public BigDecimal getTotalPrice() {
         return items.stream()
                 .map(CartItem::getTotalPrice)
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public CartItem getItem(Long productId){
+        return items.stream()
+                .filter(item -> item.getProduct().getId().equals(productId))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public CartItem addItem(Product product) {
+        var cartItem = getItem(product.getId());
+        if(cartItem != null) {
+            cartItem.setQuantity(cartItem.getQuantity() + 1);
+        }
+        else {
+            cartItem = new CartItem();
+            cartItem.setProduct(product);
+            cartItem.setQuantity(1);
+            cartItem.setCart(this);
+            items.add(cartItem);
+        }
+        return cartItem;
+    }
+
+    public void removeItem(Long productId) {
+        var productItem = getItem(productId);
+        if(productItem != null) {
+            items.remove(productItem);
+            productItem.setCart(null);
+        }
+    }
+
+    public void clear(){
+        items.clear();
     }
 
 }
